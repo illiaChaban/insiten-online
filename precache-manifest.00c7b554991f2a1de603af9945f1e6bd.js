01b19969e32b5f5525e742b3ff0b90b697dc79b8
Illia Chaban <illia.chaban.8@gmail.com>
2019-09-12 21:40:45 -0400

    added build for serving online

---
+++ precache-manifest.00c7b554991f2a1de603af9945f1e6bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c24f4251f338800f001056e656d6f50d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2052c794fe76dd52a44",
+    "url": "/static/css/main.18b0e734.chunk.css"
+  },
+  {
+    "revision": "1253f27947905ca1db9e",
+    "url": "/static/js/2.65e6ce2f.chunk.js"
+  },
+  {
+    "revision": "e2052c794fe76dd52a44",
+    "url": "/static/js/main.ff3c9209.chunk.js"
+  },
+  {
+    "revision": "f5485d9e27d7ad0f18d5",
+    "url": "/static/js/runtime~main.1ec3a495.js"
+  }
+]);
